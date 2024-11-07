17a85058f519fd44514e38595e3744137091a637
文彥 <xx48833@gmail.com>
2024-11-07 20:02:00 +0800

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-int main() {
-    int R;
-    double area,r,pi=3.1415;
-    while(scanf("%d",&R)!= -1)
-    {
-        r = (double)R/2;
-        area =r*r*pi;
-        if ((area-(int)area)==0)
-            printf("%d\n",(int)area);
-        else
-            printf("%.2f\n",area);
-    }
-    return 0;
+int main(int argc, char* argv[]) {
+	int a[3],sum;
+	while(scanf("%d %d %d",&a[0], &a[1], &a[2]) != -1) {
+		sum = 0;
+		for (int i = 0; i <= 2; i++) {
+			if (a[i] < 1911 || a[i]>2024) {
+				printf("%d不在此範圍內，",a[i]);
+			}
+			else if (a[i] % 4 == 0 && a[i] % 100 != 0) {
+				printf("%d是閏年，", a[i]);
+				sum += 1;
+			}
+			else {
+				printf("%d是平年，", a[i]);
+			}
+			
+		}
+		printf("閏年總共有%d個\n", sum);
+	}
+
+	return 0;
 }
